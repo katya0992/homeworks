673fd04b9675fde5a966fa4344f8ff70e0f45471
katya0992 <averyanova-ekaterina@ya.ru>
2017-06-24 09:23:34 +0300

    Update hw3.js

--- hw3.js
+++ hw3.js
@@ -1,76 +1,26 @@
-// let arr = function () {
-//     for (let i = 0; i < arr.length; i++) {
-//  console.log(arr[i]);
-// } 
-// }
-
-// let arr = [];
-// function getArr (n) {
-//      for (let i = 0; i < arr.length; i++) {
-//  console.log(arr[i]);
-// } 
-// return arr
-// } 
-// getArr(10);
-
-
-
-
-
-// //задача 5
-// let arr = [1,2,3,4,5];
-// let res;
-// res = arr.splice(2,2)
-
-// //задача 6
-// let arr = [1,2,3,4,5];
-// let res;
-// res = arr.splice(2,2, 'three' , 'four')
-
-// //задача 7
-// let arr = ['i' , 'learn' , 'javascript']
-// let res;
-// res = arr.splice (2,0, 'awesome')
-
-
-
-
 // //задача 1
-//  function doubleArray(arr) {
-//     var newArr = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         newArr = arr.concat(1,2,3);
-//     }
-//     return newArr;
-//  } 
-//
+
 //  function doubleArray1(arr) {
-//     return arr.concat(arr).concat(arr).join().split(',')
+//     return arr.concat(arr)
 //  }
 //  var result = doubleArray([1,2,3]); 
 // console.log(result);    
 
-// Test splice
-//var list=[1,2,3,4,5]
-
-// result=list.splice(1);
-// console.log(result); 
-
-// result=list.splice(1,10);
-// console.log(result);    
 
 
 // //задача 2
+
 // function lastEl(arr) {
 //     return arr[arr.length-1]
 // }
 // var result = lastEl([2,3,5])
 
+
+
 // задача 3
 
 // function getArray (num) {
 //     var arr = [];
-    
 //     for(var i = 1; i<=num; i++){
 //        arr.push(i);
 //     }
@@ -79,12 +29,13 @@
 // let result = getArray(10);
 // console.log(result);  
 
+
+
 // задача 4
 
 // function changeCollection () {
-//     var n= arguments.length;
-//     var arr = arguments;
-    
+//     var n= arguments.length,
+//         arr = arguments;
 //     for(var i = 0; i<n; i++){
 //        arr[i].shift();
 //     }
@@ -92,13 +43,12 @@
 // }
 // let result = changeCollection([1,2,3],['a','b','c']);
 // console.log(result);  
-// result = changeCollection([1,2,3]);
-// console.log(result);  
-// result = changeCollection([1,2,3],['a','b','c'],['a','b','c']);
-// console.log(result);  
 //-------------------------------------------
 //
+
+
 // задача 1
+
 // function reverse1 (str){ 
 //     var arr = str.split("");
 //     return arr.sort().reverse();
@@ -109,18 +59,16 @@
 
 
 //задача 2
+
 //  function sortRev(arr){ 
-
 //     return arr.sort((prev, next) => (next-prev))
-//     //    return next - prev
-
 // }
 // let result = sortRev ([2, 4, 7, 1, -2, 10, -9]);
 // console.log(result);  
 
 
-//задача 3
 
+//задача 3
 
 //  function getNewArray(arr, n, m){ 
 
@@ -142,19 +90,43 @@
 
 
 
+// //задача 5
+
+// let arr = [1,2,3,4,5];
+// let res;
+// res = arr.splice(2,2)
+
+
+
+
+// //задача 6
+
+// let arr = [1,2,3,4,5];
+// let res;
+// res = arr.splice(2,2, 'three' , 'four')
+
+
+
+
+// //задача 7
+
+// let arr = ['i' , 'learn' , 'javascript']
+// let res;
+// res = arr.splice (2,0, 'awesome')
+
+
 
 // //задача 8
+
 //  function sortByLen(arr){ 
-
 //     return arr.sort((prev, next) => (prev.length - next.length))
-
 // }
 // let result = sortByLen([[14, 45], [1], ['a', 'c', 'd']]);
 // console.log(result);  
 
 
-//задача 10
 
+//задача 10
 
 // var a =
 // [
@@ -169,45 +141,41 @@
 
 // {cpu: 'amd', info: {cores:4, сache: 2}}
 
-// ]
+// ];
 
 //  function sortByCores(arr){ 
-
 //     return arr.sort((prev, next) => (prev.info.cores - next.info.cores))
-
 // }
 // let result = sortByCores(a);
 // console.log(result);  
 
 
 
-//задача 11
+// //задача 11
 
+// let products = [
 
-let products = [
+// {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
 
-{title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+// {title: 'prod3', price: 15}, {title: 'prod4', price: 25},
 
-{title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+// {title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
 
-{title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+// {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
 
-{title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+// ];
 
-];
+//  function filterCollection(arr,min,max){ 
+//    var arr2=[];
+//     for ( let i = 0; i<arr.length; i++){
+//         let p = arr[i].price; //цена текущего продукта
+//         if (p >= min && p <= max)   {
+//             arr2.push (arr[i]);
+//               } 
+//      };
+//     return arr2.sort((prev, next) => (prev.price - next.price))
 
- function filterCollection(arr,min,max){ 
-   var arr2=[];
-    for ( let i = 0; i<arr.length; i++){
-        let p = arr[i].price; //цена текущего продукта
-        if (p >= min && p <= max)   {
-            arr2.push (arr[i]);
-              } 
-     };
-   // return arr2 
-    return arr2.sort((prev, next) => (prev.price - next.price))
-
-}
-let result = filterCollection(products, 15, 30);
-console.log("Products=" , result);  
+// }
+// let result = filterCollection(products, 15, 30);
+// console.log("Products=" , result);  
 
