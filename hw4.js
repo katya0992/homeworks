ec3a399314abda7377a8e27ab8685ce57b25f60e
katya0992 <averyanova-ekaterina@yandex.ru>
2017-06-26 07:38:38 +0300

    added hw4

---
+++ hw4.js
@@ -0,0 +1,55 @@
+// //задача 1
+
+// let products = [
+
+// {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+
+// {title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+
+// {title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+
+// {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+
+// ];
+
+// let newProducts = products.filter ((product) => product.price >= 15 && product.price <= 30);
+
+// console.log(newProducts)
+
+
+
+// // задача 2
+
+
+// var arr = [1, 2, 3, 5, 8, 9, 10]
+// var res = arr.map(function (num) {
+//             var obj = {};
+//             obj.digit = num;
+//             obj.odd = (num % 2 == 1);
+//             return obj
+//         });
+// console.log(res)
+
+
+
+// //задача 3
+
+
+// var arr = [12, 4, 50, 1, 0, 18, 40];
+
+// var res = arr.every ((element) => element == 0);
+
+// console.log(res);
+
+
+
+
+// //задача 4
+
+// var words = ['yes', 'hello', 'no', 'easycode', 'what'];
+
+// var res = words.some ((word) => word.length > 3);
+
+// console.log (res);
+
+
